6caa2e75b056d0bdbeae9199a99e6ecf20439f25
hang-1n-there <s22037@gsm.hs.kr>
2023-06-10 16:55:41 +0900

    plt.axis

---
+++ code/4.axis.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4], [5,7,9,11], label='Price ($)')
+plt.plot([1, 2, 3, 4], [3, 5, 9, 7], label='Demand (#)')
+plt.xlabel('X-label')
+plt.ylabel('Y-label')
+
+plt.legend(loc='best',ncol=2)
+plt.axis([1,4,3,11])
+
+plt.show()
--- organize.md
+++ organize.md
@@ -18,3 +18,10 @@ plt.legend(loc='범례 위치', ncol = 그래프의 수)
 - 그래프의 범례와 개수를 표시하는 함수
 - plt.plot(label='그래프의 이름')을 추가해줘야 함
 ---
+## plt.axis
+```python
+plt.axis([xmin,xmax,ymin,ymax])
+```
+- x와 y의 최솟값과 최댓값을 그래프에 적용하는 함수
+---
+
